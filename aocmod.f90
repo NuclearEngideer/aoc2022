d28a338135d9e27b00f1cba081712844c69b4b34
Avery Grieve <agrieve@lanl.gov>
2022-12-13 11:03:07 -0700

    Add string to int and int to string functions for convenience
    Signed-off-by: Avery Grieve <agrieve@lanl.gov>

--- aocmod.f90
+++ aocmod.f90
@@ -4,7 +4,7 @@ module aocmod
 
   integer(8) function countLines(filename)
     implicit none
-    character(256), intent(in) :: filename
+    character(*), intent(in) :: filename
     character(256) :: ctmp
     integer :: FID=1, ierr=0, number=0
 
@@ -18,5 +18,19 @@ module aocmod
     close(FID)
     countLines=number
   end function countLines
+
+  integer(8) function strtoint(intchar, fstring)
+    implicit none
+    character(*), intent(in) :: intchar
+    character(*), intent(in) :: fstring
+    read(intchar, fstring) strtoint
+  end function strtoint
+
+  character(256) function inttostr(intchar, fstring)
+    implicit none
+    integer(8), intent(in) :: intchar
+    character(*), intent(in) :: fstring
+    write(inttostr, fstring) intchar
+  end function inttostr
 
 end module aocmod
